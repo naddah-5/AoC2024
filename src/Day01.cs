43766afe9249186ec63e019ff568f1cfa461c7d7
Anders Dahl <andersoskardahl@hotmail.com>
2024-12-07 11:38:04 +0100

    project init

---
+++ src/Day01.cs
@@ -0,0 +1,10 @@
+﻿namespace AoC.Day01
+{
+    public class Day01
+    {
+        public bool NewMethod(bool modifier)
+        {
+            return modifier;
+        }
+    }
+}
---
+++ tests/Day01Test.cs
@@ -0,0 +1,14 @@
+﻿namespace AoC.Tests;
+
+using AoC.Day01;
+
+public class Day01Test
+{
+    [Fact]
+    public void Test1()
+    {
+        var day = new Day01();
+        var res = day.NewMethod(true);
+        Assert.True(res);
+    }
+}
