4abdf0041536b314f7a7aaad86787da5a7f53371
Anders Dahl <andersoskardahl@hotmail.com>
2024-12-10 16:35:55 +0100

    stash

--- src/Day01.cs
+++ src/Day01.cs
@@ -1,10 +1,28 @@
-﻿namespace AoC.Day01
-{
-    public class Day01
-    {
-        public bool NewMethod(bool modifier)
-        {
-            return modifier;
+﻿using System;
+using System.Text;
+using System.IO;
+
+namespace AoC.Day01 {
+    public class Day01 {
+        private List<int>? DiffList {
+             get; set; 
         }
+        // Sort the lists then 
+        
+        public Day01() {
+            
+        }
+
+        public void CalcPosition() {
+            using(FileStream fs = File.Open("input.txt", FileMode.Open, FileAccess.Read, FileShare.Read)) {
+                byte[] b = new byte[1024];
+                UTF8Encoding temp = new UTF8Encoding(true);
+
+                while ( fs.Read(b, 0, b.Length) > 0) {
+                    Console.WriteLine(temp.GetString(b));
+                }
+            }
+        }
+
     }
 }
